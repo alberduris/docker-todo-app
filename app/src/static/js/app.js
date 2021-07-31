53322c084e2f927e126da5fca0deb0cd4183fede
EC2 Default User <ec2-user@ip-172-31-27-179.eu-west-3.compute.internal>
2021-07-31 16:43:32 +0000

    improved the add button

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Amazing Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
